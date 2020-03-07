2f04948bc148ced7b5237b7e0dc4e7f0a6aa39d0
Fajfer, Corey W <cfajfer@iu.edu>
2020-03-07 02:19:43 -0500

    "Updated framework"

--- src/mainView.js
+++ src/mainView.js
@@ -7,11 +7,12 @@ class MainView extends React.Component {
     //Construct States
     this.state = { 
       active: {
+        current: "menu",
+        menu: true,
         reverse: false,
       },
       str: ""
     };
-    //Class Variables
 
     //Bind Update Methods
     this.textareaEvent = this.textareaEvent.bind(this);
@@ -22,28 +23,48 @@ class MainView extends React.Component {
     this.setState({str: event.target.value});
   }
 
-  toggleView(component){
+  toggleView(menu, component){
     switch(component){
       case "reverseEncrypt":
           this.setState(prevState => ({
             active: {
             ...prevState.active,
-            reverse: !this.state.active.reverse
+            current: "reverseEncrypt",
+            menu: !this.state.active.menu,
+            reverse: !this.state.active.reverse,
             }
           }));
           break;
     }
+    if(menu === true){
+      this.setState(prevState => ({
+        active: {
+        ...prevState.active,
+        current: "menu"
+        }
+      }));
+    }
   }
 
   render() {
-    return(
-      <div>
-        <textarea value={this.str} onChange={this.textareaEvent}></textarea>
-        <br/>
-        <button type="button" onClick={() => this.toggleView("reverseEncrypt")}>Reverse Encrypt Toggle</button>
-        {this.state.active.reverse && <Reverse translation="encrypt" subType="string" str={this.state.str}/>}
-      </div>
-    );
+    if(this.state.active.menu === true){
+      return(
+        <div>
+          <textarea value={this.state.str} onChange={this.textareaEvent}></textarea>
+          <br/>
+          <button type="button" onClick={() => this.toggleView(false, "reverseEncrypt")}>Reverse Encrypt Toggle</button>
+        </div>
+      );
+    }
+    else if(this.state.active.menu === false){
+      return(
+        <div>
+          <a class="close" onClick={() => this.toggleView(true, this.state.active.current)}></a>
+          <p>{this.state.str}</p>
+          {this.state.active.reverse && <Reverse translation="encrypt" subType="string" str={this.state.str}/>}
+        </div>
+      );
+    }
   }
 }
 
