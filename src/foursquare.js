5c80eb722bb571508b1e5d833b4c99b7cc681732
bigbatmoegan <49653903+bigbatmoegan@users.noreply.github.com>
2020-04-14 18:00:08 -0400

    Cleaned up code
    Condensed the code to remove duplicate code and more easy to follow

--- src/foursquare.js
+++ src/foursquare.js
@@ -16,173 +16,118 @@ class Foursquare extends React.Component {
       blockNum: 1 //char break number
     };
     this.str = props.str; //values{string variable}
-
+	this.normal=["a", "b", "c", "d", "e",
+				 "f", "g", "h", "i", "j",
+				 "k", "l", "m", "n", "o",
+				 "p", "r", "s", "t", "u",
+				 "v", "w", "x", "y", "z" ];
+	this.cipher1=["z", "g", "p", "t", "f",
+			  "o", "i", "h", "m", "u",
+			  "w", "d", "r", "c", "n",
+			  "y", "k", "e", "j", "a",
+			  "x", "v", "s", "b", "l"];
+	this.cipher2=["m", "f", "n", "b", "d",
+			  "c", "r", "h", "s", "a",
+			  "x", "y", "o", "g", "v",
+			  "i", "t", "u", "e", "w",
+			  "l", "j", "z", "k", "p"];
+	//looping variables
+	this.i=0;
+	this.j=this.i+1;
     //Bind Update Methods
     this.methodEvent = this.methodEvent.bind(this);
     this.blockNumEvent = this.blockNumEvent.bind(this);
   }
   
-  componentDidMount(){
+   componentDidMount(){
     this.foursquare();
   }
-
+  
   foursquare() {
-	  //Need to get the entey box to re appear
-	 if(this.options.subType==="string") { 
-	var normal=["a", "b", "c", "d", "e",
-			"f", "g", "h", "i", "j",
-			"k", "l", "m", "n", "o",
-			 "p", "r", "s", "t", "u",
-			 "v", "w", "x", "y", "z" ];
-	var cipher1=["z", "g", "p", "t", "f",
-			  "o", "i", "h", "m", "u",
-			  "w", "d", "r", "c", "n",
-			  "y", "k", "e", "j", "a",
-			  "x", "v", "s", "b", "l"];
-	var cipher2=["m", "f", "n", "b", "d",
-			  "c", "r", "h", "s", "a",
-			  "x", "y", "o", "g", "v",
-			  "i", "t", "u", "e", "w",
-			  "l", "j", "z", "k", "p"];
-    var x;
-	var localFour="";
-	var string4="";
-	this.str=this.str.toLowerCase();
-		for(x=0; x<this.str.length; x+=2) {
-			//get rid of spaces
-			this.str=this.str.replace(/ /g, "");
-			var y=x+1
-			//check if message has odd length
-			if(y===this.str.length) {
-				var first;
-				var temp1;
-				var temp2;
-				first=this.str.charAt(x);
-				if(first==="q") {
-					first="u";
-				}
-				temp1=normal.indexOf(first);
-				temp2=cipher1[temp1];
-				string4=string4+temp2;
-				
-			}
-			else {
-				var first1;
-				var second;
-				var temp3;
-				var temp4;
-				var temp5;
-				var temp6;
-				first1=this.str.charAt(x);
-				if(first1==="q") {
-					first1="u";
-				}
-				second=this.str.charAt(y);
-				if(second==="q") {
-					second="u";
-				}
-				temp3=normal.indexOf(first1);
-				temp4=cipher1[temp3];
-				temp5=normal.indexOf(second);
-				temp6=cipher2[temp5];
-				console.log(temp4);
-				console.log(temp6);
-				string4=string4+temp4+temp6;
-				console.log(string4);
-			}
-			this.setState({strFour: string4});
-		}
-	 }
-  }
-  
-  Decrypt()
-  {
-	  console.log("HI");
+	  var localFour="";
+	  //quicker to do this than retype all arrays to uppercase.
 	  this.str=this.str.toLowerCase();
-	  console.log(this.str);
-	  var undo=this.str;
-	  var normal=["a", "b", "c", "d", "e",
-		     "f", "g", "h", "i", "j",
-			 "k", "l", "m", "n", "o",
-			 "p", "r", "s", "t", "u",
-			 "v", "w", "x", "y", "z" ];
-	  var cipher1=["z", "g", "p", "t", "f",
-			  "o", "i", "h", "m", "u",
-			  "w", "d", "r", "c", "n",
-			  "y", "k", "e", "j", "a",
-			  "x", "v", "s", "b", "l"];
-	  var cipher2=["m", "f", "n", "b", "d",
-			  "c", "r", "h", "s", "a",
-			  "x", "y", "o", "g", "v",
-			  "i", "t", "u", "e", "w",
-			  "l", "j", "z", "k", "p"];
-	  var string4="";
-	  var x;
-	  for(x=0; x<undo.length; x+=2) {
-			//get rid of spaces
-			undo=undo.replace(/ /g, "");
-			var y=x+1
-			//check if message has odd length
-			if(y===undo.length) {
-				var first;
-				var temp1;
-				var temp2;
-				first=undo.charAt(x);
-				if(first==="q") {
-					first="u";
-				}
-				temp1=cipher1.indexOf(first);
-				temp2=normal[temp1];
-				string4=string4+temp2;
-				
-			}
-			else {
-				var first1;
-				var second;
-				var temp3;
-				var temp4;
-				var temp5;
-				var temp6;
-				first1=undo.charAt(x);
-				console.log(first1);
-				if(first1==="q") {
-					first1="u";
-				}
-				second=undo.charAt(y);
-				console.log(second);
-				if(second==="q") {
-					second="u";
-				}
-				temp3=cipher1.indexOf(first1);
-				temp4=normal[temp3];
-				temp5=cipher2.indexOf(second);
-				temp6=normal[temp5];
-				string4=string4+temp4+temp6;
-				console.log(string4);
-			}
-			this.setState({strFour: string4});
+	  if(this.options.translation==="encrypt") {
+		  for(this.i=0; this.i<this.str.length;this.i+=2) {
+			  this.j=this.i+1;
+			  //check if i is the last letter of the word, this
+			  //only needing to do one of the translations
+			  if(this.i===this.str.length) {
+				  var first=this.str.charAt(this.i);
+				  if(first==="q"){
+					  first="u";
+				  }
+				  var temp1=this.normal.indexOf(first);
+				  var temp2=this.cipher1[temp1];
+				  localFour=localFour+temp2;
+			  }
+			  else{
+				  var first1=this.str.charAt(this.i);
+				  if(first1==="q"){
+					  first1="u";
+				  }
+				  var second=this.str.charAt(this.j);
+				  if(second==="q"){
+					  second="u";
+				  }
+			  var temp3=this.normal.indexOf(first1);
+			  var temp4=this.cipher1[temp3];
+			  var temp5=this.normal.indexOf(second);
+			  var temp6=this.cipher2[temp5];
+			  localFour=localFour+temp4+temp6;
+			  }
+		  }
 	  }
-  }
-  
-
- 
-
-  methodEvent(event) {
-    let eventValue = event.target.value;
-	console.log(eventValue);
-    if(eventValue === "encrypt"){
-      this.options.translation = eventValue;
-	  this.foursquare();
-    }
-    if(eventValue === "decrypt")
-	{
-		console.log("HI");
+	  //decrypt
+	  else {
+		  for(this.i=0; this.i<this.str.length;this.i+=2) {
+			  this.j=this.i+1;
+			  //check if i is the last letter of the word, this
+			  //only needing to do one of the translations
+			  if(this.i===this.str.length) {
+				  var first=this.str.charAt(this.i);
+				  if(first==="q"){
+					  first="u";
+				  }
+				  var temp1=this.cipher1.indexOf(first);
+				  var temp2=this.normal[temp1];
+				  localFour=localFour+temp2;
+			  }
+			  else{
+				  var first1=this.str.charAt(this.i);
+				  if(first1==="q"){
+					  first1="u";
+				  }
+				  var second=this.str.charAt(this.j);
+				  if(second==="q"){
+					  second="u";
+				  }
+			  var temp3=this.cipher1.indexOf(first1);
+			  var temp4=this.normal[temp3];
+			  var temp5=this.cipher2.indexOf(second);
+			  var temp6=this.normal[temp5];
+			  localFour=localFour+temp4+temp6;
+			  }
+		  }
+	  }
+	this.setState({strFour: localFour});
+	  }
+	 
+	
+	methodEvent(event) {
+      let eventValue = event.target.value;
+      if(eventValue === "encrypt"){
+        this.options.translation = eventValue;
+	    this.foursquare();
+      }
+      if(eventValue === "decrypt")
+	  {
 		this.options.translation = eventValue;
-		this.Decrypt();
-	}
-  }
-
-  blockNumEvent(event) {
+		this.foursquare();
+	  }
+    }
+	
+	blockNumEvent(event) {
     let eventValue = event.target.value;
     if(isNaN(parseInt(eventValue))){
       this.options.blockNum = eventValue.toString();
@@ -190,7 +135,7 @@ class Foursquare extends React.Component {
     else{
       this.options.blockNum = parseInt(eventValue, 10);
     }
-    this.Decrypt();
+    this.foursquare();
   }
 
   render() {
